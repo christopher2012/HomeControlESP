b1292d7fd1e0f0d1e5cbaad47814a23fe021dd49
K.Wizner <crisw2012@gmail.com>
2015-12-15 20:31:42 +0100

    Handle light request.

--- HomeControlESP/HomeControlESP.ino
+++ HomeControlESP/HomeControlESP.ino
@@ -1,5 +1,32 @@
-
-//Test commit
+/*
+* Copyright (c) 2015, Majenko Technologies
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* * Redistributions of source code must retain the above copyright notice, this
+*   list of conditions and the following disclaimer.
+*
+* * Redistributions in binary form must reproduce the above copyright notice, this
+*   list of conditions and the following disclaimer in the documentation and/or
+*   other materials provided with the distribution.
+*
+* * Neither the name of Majenko Technologies nor the names of its
+*   contributors may be used to endorse or promote products derived from
+*   this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+* ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
@@ -13,6 +40,8 @@ MDNSResponder mdns;
 ESP8266WebServer server(80);
 
 const int led = 13;
+
+
 
 void handleNotFound() {
 	digitalWrite(led, 1);
@@ -29,6 +58,7 @@ void handleNotFound() {
 		message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
 	}
 
+	//Serial.println(message);
 	server.send(404, "text/plain", message);
 	digitalWrite(led, 0);
 }
@@ -36,9 +66,9 @@ void handleNotFound() {
 void setup(void) {
 	pinMode(led, OUTPUT);
 	digitalWrite(led, 0);
-	Serial.begin(115200);
+	Serial.begin(9600);
 	WiFi.begin(ssid, password);
-	Serial.println("");
+	//Serial.println("");
 
 	// Wait for connection
 	while (WiFi.status() != WL_CONNECTED) {
@@ -56,8 +86,8 @@ void setup(void) {
 		Serial.println("MDNS responder started");
 	}
 
-	server.on("/switch", switchLight);
-	server.on("/settings", setSettings);
+	server.on("/settings", settings);
+	server.on("/light", light);
 	server.on("/inline", []() {
 		server.send(200, "text/plain", "this works as well");
 	});
@@ -71,13 +101,15 @@ void loop(void) {
 	server.handleClient();
 }
 
-void setSettings() {
-	server.send(200, "text/plain", "OK");
-}
-
-void switchLight() {
-	Serial.println(server.arg("state"));
+void light() {
+	String msg = "MSG=";
+	msg += server.arg("state");
+	Serial.print(msg);
 	String message = "OK";
 
 	server.send(200, "text/plain", message);
+}
+
+void settings() {
+	server.send(200, "text/plain", "OK");
 }
